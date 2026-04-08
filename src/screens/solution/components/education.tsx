b0a58221c7dfcfa16530b308cd5c80fa93e994d3
rkdeofuf <asa47972000@naver.com>
2026-04-08 22:39:44 +0900

    Feat: add education section

---
+++ src/screens/solution/components/education.tsx
@@ -0,0 +1,7 @@
+export const Education = () => {
+    return (
+        <div className="h-47 flex justify-center bg-education-white">
+            <img src="/education.webp" className="object-cover h-full" />
+        </div>
+    )
+}
--- src/screens/solution/index.tsx
+++ src/screens/solution/index.tsx
@@ -1,10 +1,11 @@
-import { Header, Hero } from './components'
+import { Education, Header, Hero } from './components'
 
 export default function SolutionScreen() {
     return (
         <>
             <Header />
             <Hero />
+            <Education />
         </>
     )
 }
