bd56f64e841d1b52b79c20a216884a855248057a
rkdeofuf <asa47972000@naver.com>
2026-04-11 17:09:30 +0900

    Feat: add responsive design to Hero section

--- src/screens/solution/components/hero.tsx
+++ src/screens/solution/components/hero.tsx
@@ -1,4 +1,5 @@
 import { ChevronDownIcon } from '@/screens/solution/icons'
+import { cn } from '@/utils'
 
 export const Hero = () => {
     const handleContactClick = () => {
@@ -7,32 +8,81 @@ export const Hero = () => {
     }
 
     return (
-        <div className="h-125 relative w-full *:select-none">
-            <img
-                src="/education-intro.webp"
-                className="object-cover h-125 absolute top-0 left-0"
+        <div
+            className={cn(
+                'relative w-full *:select-none h-150',
+                'lg:h-125',
+                'md:h-80',
+            )}
+        >
+            <div
+                className={cn(
+                    'absolute w-full h-full top-0 left-0 bg-no-repeat bg-[url(/hero/education-mobile.webp)] bg-size-[100%_auto] bg-[#edf3fc] bg-position-[0px_0px]',
+                    'lg:bg-size-[auto_500px] lg:bg-position-[-680px_0px] lg:bg-[url(/hero/education-intro.webp)]',
+                    'md:bg-size-[auto_320px] md:bg-position-[calc(35%-133px)_0px] md:bg-[url(/hero/education-intro.webp)]',
+                )}
             />
-            <div className="w-[1130px] mx-auto relative z-10 top-[130px]">
-                <div className="flex flex-col left-8 gap-7 ">
-                    <div className="text-[30px] font-[700] font-notokr">
+            <div
+                className={cn(
+                    'mx-auto relative z-10 pl-[calc(6%+3px)] pt-[calc(15%-50px)] top-[70px]',
+                    'lg:w-full lg:pt-0 lg:pl-9 lg:top-[130px]',
+                    'md:w-full md:pt-0 md:pl-[calc(35%-251px)] md:top-[85px]',
+                )}
+            >
+                <div
+                    className={cn(
+                        'flex flex-col gap-3',
+                        'lg:gap-7',
+                        'md:gap-4',
+                    )}
+                >
+                    <div
+                        className={cn(
+                            'font-[700] font-notokr text-[6vw]',
+                            'lg:text-[30px]',
+                            'md:text-[22px]',
+                        )}
+                    >
                         기업의 혁신과 기관의 교육을 위한 <br /> xTool 레이저
                         솔루션
                     </div>
-                    <div className="w-[400px] text-[#0000009b] text-[17px] leading-[28px]">
+                    <div
+                        className={cn(
+                            'text-[#0000009b] text-[3.5vw]',
+                            'lg:text-[17px] lg:leading-[28px] lg:w-[400px]',
+                            'md:text-[14.5px] md:leading-[24px] md:w-[400px]',
+                        )}
+                    >
                         대한민국 300+ 기업 및 기관이 선택한 압도적 기술력.
                         <br />
                         비즈니스 프로토타이핑부터 차세대 메이커 교육까지, <br />
                         전문가 팀이 도입의 모든 과정을 함께합니다.
                     </div>
-                    <div className="flex font-notokr *:cursor-pointer gap-5 mt-[20px]">
+                    <div
+                        className={cn(
+                            'flex flex-col mr-auto gap-3 font-notokr *:cursor-pointer mt-[10px]',
+                            'lg:flex-row lg:gap-5 lg:mt-[20px]',
+                            'md:flex-row md:gap-3 md:mt-[5px]',
+                        )}
+                    >
                         <div
-                            className="flex gap-3 items-center bg-btn-dark text-[#ffffff] text-[15px] px-8 py-3.5 rounded-full hover:bg-btn-dark/85"
+                            className={cn(
+                                'flex text-[12px] items-center bg-btn-dark text-[#ffffff] rounded-full hover:bg-btn-dark/85 px-5 py-3 gap-2',
+                                'lg:text-[15px] lg:px-8 lg:py-3.5 lg:gap-3',
+                                'md:text-[12px] md:px-5 md:py-2 md:gap-1',
+                            )}
                             onClick={handleContactClick}
                         >
                             비즈니스 견적 상담
                             <ChevronDownIcon size={17} color="#ffffff" />
                         </div>
-                        <div className="text-[15px] text-font-gray px-8 py-3.5 rounded-full border border-font-gray] hover:bg-font-gray hover:text-[#ffffff] transition">
+                        <div
+                            className={cn(
+                                'text-font-gray text-[12px] rounded-full border border-font-gray] hover:bg-font-gray hover:text-[#ffffff] transition px-5 py-3 text-center',
+                                'lg:text-[15px] lg:px-8 lg:py-3.5',
+                                'md:text-[13px] md:px-5 md:py-3',
+                            )}
+                        >
                             카탈로그 받기
                         </div>
                     </div>
