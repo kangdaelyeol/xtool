f549838772a34f05a762d1e144859b8bb8840763
rkdeofuf <asa47972000@naver.com>
2026-04-11 20:52:11 +0900

    Feat: add trust bar responsive design

--- src/screens/solution/components/trust-bar.tsx
+++ src/screens/solution/components/trust-bar.tsx
@@ -1,28 +1,37 @@
+import { cn } from '@/utils'
+
 export const TrustBar = () => {
     return (
         <div className="w-full border-b border-b-border-gray *:select-none">
-            <div className="w-full max-w-350 px-20 mx-auto flex justify-between py-10 *:font-notokr *:whitespace-nowrap">
+            <div
+                className={cn(
+                    'w-full mx-auto flex justify-between  *:font-notokr *:break-keep',
+                    'lg:max-w-300 lg:px-10 lg:py-10',
+                    'md:max-w-300 md:px-10 md:py-10',
+                    'py-5',
+                )}
+            >
                 <div className="flex flex-col items-center basis-0 shrink grow">
-                    <div className="text-[34px] text-trust-navy font-bold tracking-wider">
+                    <div className="text-center lg:text-[34px] md:text-[34px] text-[18px] text-trust-navy font-bold tracking-wider">
                         300+
                     </div>
-                    <div className="text-[14px] mt-1.5 tracking-wider font-semibold text-font-gray-deep">
+                    <div className="text-center lg:text-[14px] md:text-[14px] text-[12px] mt-1.5 tracking-wider font-semibold text-font-gray-deep">
                         누적 도입 파트너
                     </div>
                 </div>
                 <div className="flex flex-col items-center basis-0 shrink grow">
-                    <div className="text-[34px] text-trust-navy font-bold tracking-wider">
+                    <div className="text-center lg:text-[34px] md:text-[34px] text-[18px] text-trust-navy font-bold tracking-wider">
                         KC
                     </div>
-                    <div className="text-[14px] mt-1.5 tracking-wider font-semibold text-font-gray-deep">
+                    <div className="text-center lg:text-[14px] md:text-[14px] text-[12px] mt-1.5 tracking-wider font-semibold text-font-gray-deep">
                         산업·교육 안전 인증
                     </div>
                 </div>
                 <div className="flex flex-col items-center basis-0 shrink grow">
-                    <div className="text-[30px] text-trust-navy font-bold tracking-wider">
+                    <div className="text-center lg:text-[34px] md:text-[34px] text-[17px] text-trust-navy font-bold tracking-wider">
                         1:1 담당자 지원
                     </div>
-                    <div className="text-[14px] tracking-wider mt-1.5 font-semibold text-font-gray-deep">
+                    <div className="text-center lg:text-[14px] md:text-[14px] text-[12px] tracking-wider mt-1.5 font-semibold text-font-gray-deep">
                         도입부터 설치까지 <br />
                         전담 담당자가 지원
                     </div>
