d2f32eb21f9f8b1ea8ce30cc32fc623bc8d35c52
rkdeofuf <asa47972000@naver.com>
2026-04-18 15:31:17 +0900

    Feat: add  auto scroll partner section

--- src/screens/solution/components/education.tsx
+++ src/screens/solution/components/education.tsx
@@ -1,14 +1,55 @@
+import { useEffect, useRef } from 'react'
 import { cn } from '@/utils'
 
-export const Education = () => {
+const PartnerIcon = ({ idx }: { idx: number }) => {
     return (
-        <div
-            className={cn(
-                'flex justify-center bg-education-white bg-no-repeat bg-center',
-                'h-100 bg-[url(/education/education-logos-mobile.webp)] bg-size-[auto_calc(45%+60vw)]',
-                'md:h-47 md:bg-[url(/education/education.webp)] md:bg-size-[1100px_auto]',
-                'lg:h-47 lg:bg-[url(/education/education.webp)] lg:bg-size-[1400px_auto]',
-            )}
-        />
+        <div className="h-full flex justify-center items-center shrink-0">
+            <img
+                src={`/partner/${idx}.webp`}
+                className="h-[25px] object-contain"
+            />
+        </div>
+    )
+}
+
+export const Education = () => {
+    const sliderRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        let transX = 0
+        let rafId: number
+        const moveSlider = () => {
+            transX += 2
+            if (
+                sliderRef.current &&
+                transX >= (sliderRef.current.scrollWidth + 60) / 2
+            )
+                transX = 0
+
+            if (sliderRef.current) {
+                sliderRef.current.style.transform = `translateX(${-transX}px)`
+            }
+            rafId = requestAnimationFrame(moveSlider)
+        }
+        rafId = requestAnimationFrame(moveSlider)
+
+        return () => cancelAnimationFrame(rafId)
+    }, [])
+    const list = [] as number[]
+    for (let i = 0; i < 16; i++) {
+        list.push(i % 8)
+    }
+
+    return (
+        <div className={cn('flex bg-education-white h-25 overflow-hidden')}>
+            <div
+                ref={sliderRef}
+                className="w-full h-full flex gap-[60px] *:select-none *:pointer-events-none"
+            >
+                {list.map((idx, i) => (
+                    <PartnerIcon idx={idx} key={i} />
+                ))}
+            </div>
+        </div>
     )
 }
